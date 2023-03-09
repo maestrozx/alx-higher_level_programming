dc4ec20c8e582a089ad5a35b7c7a0abdf13d9c8f
Liberius <liberiusongere@gmail.com>
2023-03-09 23:48:34 +0300

    Modules

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+import sys
+args = sys.argv[1:]
+num_args = len(args)
+print("Number of argument{}: {}".format('s' if num_args != 1 else '', num_args), end='')
+if num_args == 0:
+    print('.')
+else:
+    print(':')
+    for i, arg in enumerate(args):
+        print("{}: {}".format(i+1, arg))
