dbf24de843f70d2bbf14b87523c6aa8e93af9c4c
maestrozx <liberiusongere@gmail.com>
2024-06-18 23:36:13 +0300

    Rectangle X

--- 0x13-javascript_objects_scopes_closures/3-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/3-rectangle.js
@@ -6,13 +6,14 @@ class Rectangle {
       this.height = h;
     }
   }
+
   print () {
     for (let i = 0; i < this.height; i++) {
-      let s = '';
+      let m = '';
       for (let j = 0; j < this.width; j++) {
-        s += 'X';
+        m += 'X';
       }
-      console.log(s);
+      console.log(m);
     }
   }
 }
